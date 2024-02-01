3234dcfbb35831c2ccacd506f9b2f08d21cadf61
SnailMail500 <alex_snell@icloud.com>
2024-02-01 22:23:18 +0000

    Added and sorted parameters for the playGame subroutine as well as sorting out some logic error-y stuff

--- C++ NoughtsAndCrosses/C++ NoughtsAndCrosses.cpp
+++ C++ NoughtsAndCrosses/C++ NoughtsAndCrosses.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+void playGame(string nameOne, string nameTwo, string symbolOne, string symbolTwo, string gameBoard) {
+	cout << "This statement is only here to stop any complaints if I forget to finish this project and run this in like 6 months' time";
+}
+
 string getSymbols(string playerName) {
 	bool valid = false;
 	string symbol = "";
@@ -38,5 +42,6 @@ int main() {
 	string playerTwo = getNames();
 	string symbolOne = getSymbols(playerOne);
 	string symbolTwo = getSymbols(playerTwo);
+	playGame(playerOne, playerTwo, symbolOne, symbolTwo, gameBoard[3][3]);
 	return 0;
 }
