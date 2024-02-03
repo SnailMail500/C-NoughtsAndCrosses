02cf402a660f8e339369c2029a97e5425523f590
SnailMail500 <alex_snell@icloud.com>
2024-02-03 16:36:18 +0000

    A desperate attempt to make the board output properly

--- C++ NoughtsAndCrosses/C++ NoughtsAndCrosses.cpp
+++ C++ NoughtsAndCrosses/C++ NoughtsAndCrosses.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void drawBoard(string board[6][6]) {
 	for (int i = 0; i < 7; i++) {
 		for (int j = 0; j < 7; j++) {
-			cout << board[i][j];
+			cout << board[j][i];
 		}
 		cout << "\n";
 	}
